0939b0a2f1e87355dc066504a9c285575bc59c03
Neto-Jaime <jaiminho.dev@gmail.com>
2024-04-14 21:31:38 -0300

    commit continuidade tarefa

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { HousingLocation } from '../housinglocation';
+import {RouterModule } from '@angular/router';
+
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports:[
+    CommonModule, RouterModule
+  ],
+  template: `
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="exerior photo of {{ housingLocation.name }}"
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">
+        {{ housingLocation.city }}, {{ housingLocation.state }}
+      </p>
+      <a [routerLink]="['/details', housingLocation.id]">Ver mais</a>
+    </section>
+  `,
+  styleUrls: ['./housing-location.component.css'],
+})
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
+}
