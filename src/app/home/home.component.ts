34e9becd8cb12aa210f0040dec949e7b4ce75a17
Jaime Neto <jaiminho.dev@gmail.com>
2024-04-18 20:02:45 -0300

    App Location housing finish

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,14 +9,25 @@ import { HousingService } from '../housing.service';
   imports: [CommonModule, HousingLocationComponent],
   template: `
     <section>
-      <form>
-        <input type="text" placeholder="Filtro por cidade" />
-        <button class="primary" type="button">Pesquisar</button>
+      <form (submit.prevent)="filterResults(filtro.value, $event)">
+        <input
+          type="text"
+          placeholder="Filtro por cidade"
+          #filtro
+          (keydown.enter)="filterResults(filtro.value, $event)"
+        />
+        <button
+          class="primary"
+          type="button"
+          (click)="filterResults(filtro.value, $event)"
+        >
+          Pesquisar
+        </button>
       </form>
     </section>
     <section class="results">
       <app-housing-location
-        *ngFor="let housingLocation of housingLocationList"
+        *ngFor="let housingLocation of filteredLocationList"
         [housingLocation]="housingLocation"
       >
       </app-housing-location>
@@ -27,8 +38,31 @@ import { HousingService } from '../housing.service';
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
-  
+
+  filteredLocationList: HousingLocation[] = [];
+
+  filterResults(text: string, event: any) {
+    event.preventDefault(); // Evita a submissão do formulário
+    console.log('Valor do filtro:', text);
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+    this.filteredLocationList = this.housingLocationList.filter(
+      (housingLocationList) =>
+        housingLocationList?.city
+          .toLocaleLowerCase()
+          .includes(text.toLowerCase())
+    );
+  }
+
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.housingService
+      .getAllHousingLocations()
+      .then((housingLocationList: HousingLocation[]) => {
+        this.housingLocationList = housingLocationList;
+
+        this.filteredLocationList = housingLocationList;
+      });
   }
 }
